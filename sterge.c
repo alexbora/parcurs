d5d18c7c843c6c70aa919423dbb65e57b4e0ba71
alexbora <alexbora@gmail.com>
2022-04-13 10:52:06 +0300

    feats - miercuri 13 aprilie 2022, 10:52:06 +0300

--- sterge.c
+++ sterge.c
@@ -25,8 +25,7 @@ char *tmp_luna;
 
 #define MAX_DAYS 32
 
-static void date_now(void)
-{
+static void date_now(void) {
   const time_t t = time(0);
   struct tm *tm = gmtime(&t);
   strftime(longdate, 64, "%d.%m.%Y", tm);
@@ -40,8 +39,7 @@ static void date_now(void)
   tm = NULL;
 }
 
-static void date_prev(int year, int mon, int day)
-{
+static void date_prev(int year, int mon, int day) {
   struct tm ti = {59, 59, 12, day, mon - 2, year - 1900};
   time_t epoch = mktime(&ti);
   /* printf("%s", asctime(gmtime(&epoch))); */
@@ -50,11 +48,11 @@ static void date_prev(int year, int mon, int day)
   TM_PREV = ti;
   TM_PREV.tm_year += 1900;
   TM_PREV.tm_mon++;
-  if (TM_PREV.tm_mon == 0) TM_PREV.tm_year--;
+  if (TM_PREV.tm_mon == 0)
+    TM_PREV.tm_year--;
 }
 
-static void date_cmdl(int year, int mon, int day)
-{
+static void date_cmdl(int year, int mon, int day) {
   if (year < 1970) {
     puts("Invalid year\n");
     exit(EXIT_SUCCESS);
@@ -72,22 +70,19 @@ static void date_cmdl(int year, int mon, int day)
   TM = tm;
 }
 
-static void get_km(double *km)
-{
+static void get_km(double *km) {
   FILE *f = fopen("km", "r");
   fscanf(f, "%lf", km);
   fclose(f);
   f = NULL;
 }
-static void write_km(double *km)
-{
+static void write_km(double *km) {
   FILE *f = fopen("km", "w++");
   fprintf(f, "%lf", *km);
   fclose(f);
   f = NULL;
 }
-static void process_cmdl(int argc, char *argv[restrict argc + 1])
-{
+static void process_cmdl(int argc, char *argv[restrict argc + 1]) {
   if (argv[1] && *argv[1] == 'h') {
     puts("\nExecute like './prog year month day km', for example './prog "
          "2022 4 10 100'.\nIf 0 km, file km is read.\nIf no arguments, current "
@@ -106,39 +101,25 @@ static void process_cmdl(int argc, char *argv[restrict argc + 1])
 
 static int is_weekend(int day) { return (day == 6) | (day == 0); }
 
-static int is_holiday_static(int year, int month, int day)
-{
-  if (year != 2022) return 0;
+static int is_holiday_static(int year, int month, int day) {
+  if (year != 2022)
+    return 0;
   static const struct {
     unsigned day, month;
   } hol[] = {
-      { 1,  1},
-      { 2,  1},
-      {24,  1},
-      {22,  4},
-      {24,  4},
-      {25,  4},
-      { 1,  5},
-      { 1,  5},
-      { 1,  6},
-      {12,  6},
-      {13,  6},
-      {15,  8},
-      {30, 11},
-      { 1, 12},
-      {25, 12},
-      {26, 12},
+      {1, 1}, {2, 1},  {24, 1}, {22, 4}, {24, 4},  {25, 4}, {1, 5},   {1, 5},
+      {1, 6}, {12, 6}, {13, 6}, {15, 8}, {30, 11}, {1, 12}, {25, 12}, {26, 12},
   };
 
   for (unsigned i = 0; i < sizeof(hol) / sizeof(hol[0]); i++)
-    if (month == hol[i].month && day == hol[i].day) return 1;
+    if (month == hol[i].month && day == hol[i].day)
+      return 1;
   return 0;
 }
 
 static int is_holiday_net(int year, int month, int day) { return 0; }
 
-static unsigned days_in_month(const int month, const int year)
-{
+static unsigned days_in_month(const int month, const int year) {
   if (month == 4 || month == 6 || month == 9 || month == 11)
     return 30;
   else if (month == 2)
@@ -147,8 +128,7 @@ static unsigned days_in_month(const int month, const int year)
   return 31;
 }
 
-static char *literal_mon(int month)
-{
+static char *literal_mon(int month) {
   return &"\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0ianuarie\0\0\0\0\0\0\0\0februari"
           "e\0\0\0"
           "\0\0\0\0martie\0\0\0\0\0\0\0\0\0\0aprilie\0\0\0\0\0\0\0\0\0mai\0"
@@ -165,8 +145,7 @@ static char *literal_mon(int month)
 void fn(void) { puts("fn\n"); }
 void fe(void) { puts("fe\n"); }
 
-void generate_array(int *arr)
-{
+void generate_array(int *arr) {
   /* arr[0] = 9; */
   /* int *p = arr; */
   /* printf("%d\n\n", *(p++)); */
@@ -185,8 +164,15 @@ void generate_array(int *arr)
   }
 }
 
-int main(int argc, char *argv[argc + 1])
-{
+void generate_time(int argc, char *argv[]) {
+  setlocale(LC_TIME, "ro_RO.UTF-8");
+  process_cmdl(argc, argv);
+  int net = 0;
+  is_holiday = net ? is_holiday_net : is_holiday_static;
+  generate_array(array);
+}
+
+int main(int argc, char *argv[argc + 1]) {
   setlocale(LC_TIME, "ro_RO.UTF-8");
   process_cmdl(argc, argv);
 
@@ -235,7 +221,9 @@ int main(int argc, char *argv[argc + 1])
   /* } */
 
   /* return 0; */
-  for (unsigned i = 1; i <= 10; ++i) { fp[i](); }
+  for (unsigned i = 1; i <= 10; ++i) {
+    fp[i]();
+  }
 
   /* printf("%d\n", row[1]); */
 
