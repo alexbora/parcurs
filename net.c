06bce768170515b705865083a4a88d866b8e7fe3
alex <alexbora@gmail.com>
2022-04-16 13:15:44 +0300

    cleaning

--- net.c
+++ net.c
@@ -17,14 +17,27 @@
 #include <sys/socket.h> /* socket, connect */
 #include <unistd.h>
 
-static ssize_t fetch(char *buf, const int year) {
-  struct addrinfo hints = {.ai_family = AF_INET,
+static ssize_t fetch(char *buf, const int year)
+{
+
+  struct hostent *he =
+      gethostbyname("us-central1-romanian-bank-holidays.cloudfunctions.net");
+  struct sockaddr_in addr = {.sin_family      = AF_INET,
+                             .sin_port        = htons(80),
+                             .sin_addr.s_addr = *(long *)(he->h_addr_list[0])};
+
+  int sd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (connect(sd, (struct sockaddr *)&addr, sizeof(addr)))
+    return 0;
+
+  struct addrinfo hints = {.ai_family   = AF_INET,
                            .ai_socktype = SOCK_STREAM,
                            .ai_protocol = IPPROTO_TCP,
-                           .ai_flags = AI_PASSIVE},
-                  *res = NULL;
+                           .ai_flags    = AI_PASSIVE},
+                  *res  = NULL;
 
-  const char *restrict const host =
+  const char *const host =
       "us-central1-romanian-bank-holidays.cloudfunctions.net";
   int x = getaddrinfo(host, "80", &hints, &res);
   if (x)
@@ -38,7 +51,7 @@ static ssize_t fetch(char *buf, const int year) {
     return 0;
   puts("\n\x1b[32mConnected.\x1b[0m\n");
 
-  char header[256] = {'\0'};
+  char      header[256] = {'\0'};
   const int len_header =
       sprintf(header,
               "GET /romanian_bank_holidays/?year=%d HTTP/1.1\r\nHost: "
@@ -49,7 +62,7 @@ static ssize_t fetch(char *buf, const int year) {
   if (sent <= 0)
     return 0;
 
-  char *p = buf;
+  char         *p        = buf;
   const ssize_t received = recv(sockfd, p, 4 * 1024, 0);
   if (received < 1)
     return 0;
@@ -60,26 +73,29 @@ static ssize_t fetch(char *buf, const int year) {
   return received;
 }
 
-static char *parse(char *in) {
+static char *parse(char *in)
+{
   while (*in++ != '[')
     ;
   return in;
 }
 
-static void fill_struct(char *in, struct Net *h) {
+static void fill_struct(char *in, struct Net *h)
+{
   char *x = in;
-  int i = 0;
+  int   i = 0;
   while (x++) {
     x = strstr(x, "date");
     if (!x)
       break;
-    h[i].day = atoi(x + 7);
+    h[i].day   = atoi(x + 7);
     h[i].month = atoi(x + 10);
     i++;
   }
 }
 
-void net_fetch() {
+void net_fetch()
+{
   char *buf = malloc(4096);
   fetch(buf, current_year);
   char *result = parse(buf);
