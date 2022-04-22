fe8c6a34c473404ef8c73b27ccaedad4d4c2c751
alexbora <alexbora@gmail.com>
2022-04-22 11:29:37 +0300

    mix - vineri 22 aprilie 2022, 11:29:37 +0300

--- excel.c
+++ excel.c
@@ -4,8 +4,8 @@
  * @created     : Marţi Apr 19, 2022 19:11:39 EEST
  */
 
-#include "date.h"
 #include "excel.h"
+#include "date.h"
 #include "main.h"
 
 #include <stddef.h>
@@ -20,34 +20,27 @@ void (*fp[32])(lxw_worksheet *, uint32_t *, const uint16_t, const char *,
                lxw_format *);
 
 static void wkend(lxw_worksheet *s, uint32_t *row, const uint16_t col,
-                  const char *text, lxw_format *f)
-{
+                  const char *text, lxw_format *f) {
   (void)text;
   worksheet_write_string(s, *row, col, "", f);
   (*row)++;
 }
 
 static void wday(lxw_worksheet *s, uint32_t *row, const uint16_t col,
-                 const char *text, lxw_format *f)
-{
+                 const char *text, lxw_format *f) {
   worksheet_write_string(s, *row, col, text, f);
   worksheet_write_number(s, *row, col + 1, 1, f);
   worksheet_write_string(s, *row, col + 2, "inte", f);
   (*row)++;
 }
 
-void fn(struct Route *p)
-{
+void fn(struct Route *p) {
   puts(p->route);
   puts("\n");
 };
-void fnull(struct Route *p)
-{
-  puts("free\n");
-};
+void fnull(struct Route *p) { puts("free\n"); };
 
-void prepare_work()
-{
+void prepare_work() {
   struct Work {
     struct Route r;
     void (*f)(struct Route *);
@@ -57,7 +50,7 @@ void prepare_work()
     if (array[i])
       wa[i] = (struct Work){route_[i], .f = fnull};
     else
-      wa[i] = (struct Work){route_[i], .f = fn};
+      wa[i] = (struct Work){0, .f = fn};
   }
 
   for (unsigned i = 0; i < dayz; i++)
@@ -100,15 +93,14 @@ void prepare_work()
 /*   } */
 /* } */
 
-void write_excel(void)
-{
+void write_excel(void) {
 
   prepare_work();
 
   /* set data */
-  const uint32_t row         = 0;
+  const uint32_t row = 0;
   const unsigned daysinmonth = dayz;
-  unsigned       total = 0, parcursi = 0, offset = 13;
+  unsigned total = 0, parcursi = 0, offset = 13;
 
   char name[128], worksheet_name[128], data_predarii[128];
   sprintf(name, "foaie_parcurs_B-151-VGT_%s_%d_Alex_Bora.xlsx", luna,
@@ -118,27 +110,27 @@ void write_excel(void)
           longdate);
 
   lxw_workbook_options options = {.constant_memory = LXW_FALSE,
-                                  .tmpdir          = getcwd(NULL, 0),
-                                  .use_zip64       = LXW_TRUE};
+                                  .tmpdir = getcwd(NULL, 0),
+                                  .use_zip64 = LXW_TRUE};
 
   /* set properties */
   lxw_doc_properties properties = {
-      .title    = name,
-      .subject  = "foaie",
-      .author   = "Alex Bora",
-      .manager  = "tot el",
-      .company  = "Volvo",
+      .title = name,
+      .subject = "foaie",
+      .author = "Alex Bora",
+      .manager = "tot el",
+      .company = "Volvo",
       .category = "foaie parcurs",
       .keywords = "foaie parcurs",
       .comments = "VERSION 2.0",
-      .status   = "Done",
+      .status = "Done",
   };
 
   lxw_data_validation *data_validation =
-      &(lxw_data_validation){.validate     = LXW_VALIDATION_TYPE_ANY,
-                             .criteria     = LXW_VALIDATION_TYPE_ANY,
+      &(lxw_data_validation){.validate = LXW_VALIDATION_TYPE_ANY,
+                             .criteria = LXW_VALIDATION_TYPE_ANY,
                              .ignore_blank = LXW_VALIDATION_OFF,
-                             .show_input   = LXW_VALIDATION_OFF};
+                             .show_input = LXW_VALIDATION_OFF};
   /* open workbook */
   lxw_workbook *workbook = workbook_new_opt(name, &options);
   workbook_set_properties(workbook, &properties);
@@ -166,11 +158,11 @@ void write_excel(void)
   worksheet_insert_image(worksheet, row + 1, COL4, "logo.png");
 
   /* add formats */
-  lxw_format *format_bold       = workbook_add_format(workbook);
+  lxw_format *format_bold = workbook_add_format(workbook);
   lxw_format *format_bold_right = workbook_add_format(workbook);
-  lxw_format *format_header     = workbook_add_format(workbook);
-  lxw_format *format            = workbook_add_format(workbook);
-  lxw_format *format_footer     = workbook_add_format(workbook);
+  lxw_format *format_header = workbook_add_format(workbook);
+  lxw_format *format = workbook_add_format(workbook);
+  lxw_format *format_footer = workbook_add_format(workbook);
   format_set_bold(format_bold);
   format_set_border(format_bold, LXW_BORDER_NONE);
   format_set_align(format_bold_right, LXW_ALIGN_RIGHT);
