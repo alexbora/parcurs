6c70272291214fb0c443a72847e36d1566aca0b7
alexbora <alexbora@gmail.com>
2022-04-29 18:55:53 +0300

    mix - vineri 29 aprilie 2022, 18:55:53 +0300

--- SCRATCH/sterge13.c
+++ SCRATCH/sterge13.c
@@ -12,11 +12,22 @@
 #endif
 #include <time.h>
 
-static const char *mths = "ian feb mar apr mai iun iul aug sep oct noi dec";
-static char        longdate[128], *luna;
+static inline char *literal_mon(const int month) {
+  return &"ianuarie\0\0\0\0\0\0\0\0februari"
+          "e\0\0\0"
+          "\0\0\0\0martie\0\0\0\0\0\0\0\0\0\0aprilie\0\0\0\0\0\0\0\0\0mai\0"
+          "\0\0\0\0"
+          "\0\0\0\0\0\0\0\0iunie\0\0\0\0\0\0\0\0\0\0\0iulie\0\0\0\0\0\0\0\0"
+          "\0\0\0au"
+          "gust\0\0\0\0\0\0\0\0\0\0septembrie\0\0\0\0\0\0octombrie\0\0\0\0"
+          "\0\0\0noi"
+          "embrie\0\0\0\0\0\0\0decembrie\0\0\0\0\0\0\0"[16 * month];
+}
 
-static inline unsigned days_in_month(const int month, const int year)
-{
+static const char *mths = "ian feb mar apr mai iun iul aug sep oct noi dec";
+static char longdate[128], *luna;
+
+static inline unsigned days_in_month(const int month, const int year) {
   if (month == 4 || month == 6 || month == 9 || month == 11)
     return 30;
   else if (month == 2)
@@ -25,8 +36,7 @@ static inline unsigned days_in_month(const int month, const int year)
   return 31;
 }
 
-static int now()
-{
+static int now() {
   /* normal time */
   struct tm tm = *localtime(&(time_t){time(NULL)});
   /* printf("Today is           %s", asctime(&tm)); */
@@ -40,31 +50,32 @@ static int now()
   /* adjust year to jump to previous if december */
   tm.tm_year = tm.tm_mon != 11 ? tm.tm_year : tm.tm_year - 1;
   mktime(&tm); // tm_isdst is not set to -1; today's DST status is used
+  luna = literal_mon(tm.tm_mon);
 
   return 1;
 }
 
-static int then(char **argv)
-{
-  char     *m   = strstr(mths, argv[1]);
+static int then(char **argv) {
+  char *m = strstr(mths, argv[1]);
   struct tm tm2 = {50, 50, 12, 1, (int)((m - mths) / 4), 2000 + atoi(argv[2])};
   mktime(&tm2);
   sprintf(longdate, "%02d.%02d.%d", tm2.tm_mday, tm2.tm_mon + 1, tm2.tm_year);
+  luna = literal_mon(tm2.tm_mon);
   return 1;
 }
 
-int cmdl(int argc, char **argv)
-{
+int cmdl(int argc, char **argv) {
   if (argc > 2)
     return then(argv);
   return now();
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
   cmdl(argc, argv);
 
   puts(longdate);
+
+  printf("%s\n", luna);
   return 0;
 }
