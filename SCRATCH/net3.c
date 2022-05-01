02728fce0a467beb142e78d17dabb09cd107e333
alexbora <alexbora@gmail.com>
2022-05-01 19:12:50 +0300

    mix - duminică 1 mai 2022, 19:12:50 +0300

--- SCRATCH/net3.c
+++ SCRATCH/net3.c
@@ -1,3 +1,17 @@
+
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/x509.h>
+#include <openssl/x509_vfy.h>
+#include <resolv.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 #define __POSIX_C_SOURCE = 200112L
 #include <arpa/inet.h>
 #include <assert.h>
@@ -15,6 +29,17 @@
 
 #define CON_MSG "\n\x1b[32mConnected.\x1b[0m\n"
 #define CONNECT_VERBOSE 1
+
+void InitializeSSL() {
+  SSL_load_error_strings();
+  SSL_library_init();
+  OpenSSL_add_all_algorithms();
+}
+
+void DestroySSL() {
+  ERR_free_strings();
+  EVP_cleanup();
+}
 
 static ssize_t fetch(char *buf, const int year, const int flags, SSL *ssl) {
   struct addrinfo hints = {.ai_family = AF_INET,
@@ -40,21 +65,8 @@ static ssize_t fetch(char *buf, const int year, const int flags, SSL *ssl) {
   int sockfd = -1;
   sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
-  if (sockfd < 0)
-    return 0;
-
   SSL_set_fd(ssl, sockfd);
-  if (SSL_connect(ssl) == 1)
-    puts("connect\n");
-
-  x = connect(sockfd, res->ai_addr, res->ai_addrlen);
-  if (x)
-    return 0;
-
-  if (flags & CONNECT_VERBOSE) {
-    fprintf(stderr, "done.");
-    fprintf(stderr, CON_MSG);
-  }
+  SSL_connect(ssl);
 
   char header[256] = {'\000'};
   const int len_header = sprintf(
@@ -65,28 +77,12 @@ static ssize_t fetch(char *buf, const int year, const int flags, SSL *ssl) {
       "year=%d HTTP/1.1\r\nHost: calendarific.com\r\n\r\n",
       year);
 
-  int err = SSL_write(ssl, header, len_header);
-  printf("%d\n", err);
-  int e = SSL_get_error(ssl, err);
-  printf("%d\n", e);
-
-  const ssize_t sent = send(sockfd, header, (size_t)len_header, 0);
-  {
-    if (sent <= 0)
-      return 0;
-  }
+  SSL_write(ssl, header, len_header);
 
   char *p = buf;
 
   SSL_read(ssl, p, 4096);
-  puts(" ---\n ");
   puts(p);
-  /* const ssize_t received = recv(sockfd, p, 4 * 1024, 0); */
-  /* if (received < 1) */
-  /* return 0; */
-
-  if (flags & CONNECT_VERBOSE)
-    fprintf(stderr, "%s\n", p);
 
   /* safety */
   memset(res, 0, sizeof(struct addrinfo));
@@ -101,6 +97,7 @@ static ssize_t fetch(char *buf, const int year, const int flags, SSL *ssl) {
 
 int main(int argc, char *argv[]) {
   SSL_library_init();
+  InitializeSSL();
   OpenSSL_add_all_algorithms();
   SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
   SSL_CTX_set_options(ctx, SSL_OP_SINGLE_DH_USE);
@@ -108,6 +105,5 @@ int main(int argc, char *argv[]) {
 
   char buf[4096];
   fetch(buf, 2022, CONNECT_VERBOSE, ssl);
-  puts(buf);
   return 0;
 }
