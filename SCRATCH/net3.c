6934c2dcf5533ce79a443ba8ff3d1e679624ae22
alexbora <alexbora@gmail.com>
2022-05-01 20:21:26 +0300

    mix - duminică 1 mai 2022, 20:21:26 +0300

--- SCRATCH/net3.c
+++ SCRATCH/net3.c
@@ -30,30 +30,19 @@
 #define CON_MSG "\n\x1b[32mConnected.\x1b[0m\n"
 #define CONNECT_VERBOSE 1
 
-void InitializeSSL() {
-  SSL_load_error_strings();
-  SSL_library_init();
-  OpenSSL_add_all_algorithms();
-}
+const char *const host = "example.org";
 
-void DestroySSL() {
-  ERR_free_strings();
-  EVP_cleanup();
-}
-
-static ssize_t fetch(char *buf, const int year, const int flags, SSL *ssl) {
+static int sock(const int flags) {
   struct addrinfo hints = {.ai_family = AF_INET,
                            .ai_socktype = SOCK_STREAM,
                            .ai_protocol = IPPROTO_TCP,
                            .ai_flags = AI_PASSIVE},
                   *res = NULL;
 
-  const char *const host = "www.calendarific.com";
-
   if (flags & CONNECT_VERBOSE)
     fprintf(stderr, "Looking up %s ... ", host);
 
-  int x = getaddrinfo(host, "443", &hints, &res);
+  int x = getaddrinfo(host, "80", &hints, &res);
   if (x) {
     puts(gai_strerror(x));
     return 0;
@@ -64,46 +53,41 @@ static ssize_t fetch(char *buf, const int year, const int flags, SSL *ssl) {
 
   int sockfd = -1;
   sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+  int c = connect(sockfd, res->ai_addr, res->ai_addrlen);
+  if (sockfd == 3 && flags & CONNECT_VERBOSE)
+    fprintf(stderr, "done.\n");
 
-  SSL_set_fd(ssl, sockfd);
-  SSL_connect(ssl);
-
-  char header[256] = {'\000'};
-  const int len_header = sprintf(
-      header,
-      "GET "
-      "/api/v2/"
-      "holidays?&api_key=6354938168d76b7df8711085ba5dd4aab9759cd2&country=RO&"
-      "year=%d HTTP/1.1\r\nHost: calendarific.com\r\n\r\n",
-      year);
-
-  SSL_write(ssl, header, len_header);
-
-  char *p = buf;
-
-  SSL_read(ssl, p, 4096);
-  puts(p);
-
+  char *w = "GET HTTP/1.1\r\nHost: example.org\r\n\r\n";
+  write(sockfd, w, strlen(w));
+  char buf[4096];
+  read(sockfd, buf, 4096);
+  puts(buf);
   /* safety */
   memset(res, 0, sizeof(struct addrinfo));
   freeaddrinfo(res);
   res = NULL;
 
-  shutdown(sockfd, SHUT_RDWR);
-  close(sockfd);
-  return 0;
-  /* return received; */
+  return sockfd;
 }
 
 int main(int argc, char *argv[]) {
   SSL_library_init();
-  InitializeSSL();
   OpenSSL_add_all_algorithms();
-  SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
-  SSL_CTX_set_options(ctx, SSL_OP_SINGLE_DH_USE);
+  SSL_CTX *ctx = SSL_CTX_new(SSLv23_client_method());
+  SSL_CTX_set_options(ctx, SSL_OP_SINGLE_DH_USE | SSL_OP_NO_TLSv1_3);
   SSL *ssl = SSL_new(ctx);
 
+  int s = sock(CONNECT_VERBOSE);
+
+  send(s, "GET host HTTP/1.1\r\n\r\n", strlen("GET host HTTP/1.1\r\n\r\n"), 0);
+  /* SSL_set_fd(ssl, s); */
+  /* SSL_write(ssl, "GET host HTTP/1.1\r\n\r\n", */
+  /* strlen("GET host HTTP/1.1\r\n\r\n")); */
+
   char buf[4096];
-  fetch(buf, 2022, CONNECT_VERBOSE, ssl);
+  recv(s, buf, 4096, 0);
+  /* SSL_read(ssl, buf, 4096); */
+
+  puts(buf);
   return 0;
 }
