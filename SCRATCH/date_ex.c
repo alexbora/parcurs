9cf09d0373ca56efb67fbd1a9ae0a026bc71e7cc
alexbora <alexbora@gmail.com>
2022-05-07 13:41:49 +0300

    mix - sâmbătă 7 mai 2022, 13:41:49 +0300

--- SCRATCH/date_ex.c
+++ SCRATCH/date_ex.c
@@ -35,13 +35,12 @@
 time_t ti;
 
 static const char *mths = "ian feb mar apr mai iun iul aug sep oct noi dec";
-static char        longdate[128], *luna;
-static unsigned    dayz;
-static struct tm   TM, tmx[32];
-static int         days_past;
+static char longdate[128], *luna;
+static unsigned dayz;
+static struct tm TM, tmx[32];
+static int days_past;
 
-static inline char *literal_mon(const int month)
-{
+static inline char *literal_mon(const int month) {
   return &"ianuarie\0\0\0\0\0\0\0\0februari"
           "e\0\0\0"
           "\0\0\0\0martie\0\0\0\0\0\0\0\0\0\0aprilie\0\0\0\0\0\0\0\0\0mai\0"
@@ -65,21 +64,18 @@ static inline char *literal_mon(const int month)
 /*   return 31; */
 /* } */
 
-static inline int is_leap3(const int year)
-{
+static inline int is_leap3(const int year) {
   unsigned y = year + 16000;
   return (y % 100) ? !(y % 4) : !(y % 16);
 }
 
-static inline int last_day_of_mon(int year, int mon)
-{
+static inline int last_day_of_mon(int year, int mon) {
   return mon != 2 ? ((mon ^ (mon >> 3))) | 30 : is_leap3(year) ? 29 : 28;
 }
 
-static inline int days_from_civil(int y, unsigned m, unsigned d)
-{
+static inline int days_from_civil(int y, unsigned m, unsigned d) {
   y -= m <= 2;
-  const int      era = (y >= 0 ? y : y - 399) / 400;
+  const int era = (y >= 0 ? y : y - 399) / 400;
   const unsigned yoe = (y - era * 400); // [0, 399]
   const unsigned doy =
       (153 * (m > 2 ? m - 3 : m + 9) + 2) / 5 + d - 1;        // [0, 365]
@@ -87,13 +83,9 @@ static inline int days_from_civil(int y, unsigned m, unsigned d)
   return era * 146097 + doe - 719468;
 }
 
-static inline unsigned weekday_from_days(unsigned z)
-{
-  return (z + 4) % 7;
-}
+static inline unsigned weekday_from_days(unsigned z) { return (z + 4) % 7; }
 
-static int now()
-{
+static int now() {
   /* normal time */
   struct tm tm = *localtime(&(time_t){time(NULL)});
   /* printf("Today is           %s", asctime(&tm)); */
@@ -108,50 +100,45 @@ static int now()
   tm.tm_year = tm.tm_mon != 11 ? tm.tm_year : tm.tm_year - 1;
   mktime(&tm); // tm_isdst is not set to -1; today's DST status is used
 
-  TM     = tm;
+  TM = tm;
   tmx[0] = tm;
 
   days_past = ti / ONE_DAY;
   return 1;
 }
 
-static int then(char **argv)
-{
-  char     *m    = strstr(mths, argv[1]);
-  int       mon  = (int)(m - mths) / 4;
-  int       year = 2000 + atoi(argv[2]);
-  struct tm tm2  = {50, 50, 12, 1, mon, year};
+static int then(char **argv) {
+  char *m = strstr(mths, argv[1]);
+  int mon = (int)(m - mths) / 4;
+  int year = 2000 + atoi(argv[2]);
+  struct tm tm2 = {50, 50, 12, 1, mon, year};
   mktime(&tm2);
   sprintf(longdate, "%02d.%02d.%d", tm2.tm_mday, tm2.tm_mon + 1, tm2.tm_year);
 
-  TM        = tm2;
-  tmx[0]    = tm2;
+  TM = tm2;
+  tmx[0] = tm2;
   days_past = days_from_civil(year, mon, 1);
   return 1;
 }
 
-static int cmdl(int argc, char **argv)
-{
+static int cmdl(int argc, char **argv) {
   if (argc > 2)
     return then(argv);
   return now();
 }
 
-static void globals()
-{
+static void globals() {
   luna = literal_mon(TM.tm_mon);
   /* dayz = days_in_month(TM.tm_mon + 1, TM.tm_year); */
   dayz = last_day_of_mon(TM.tm_year, TM.tm_mon + 1);
 }
 
-__attribute__((noreturn)) static void usage()
-{
+__attribute__((noreturn)) static void usage() {
   puts("Usage: <mon> <year> <km>");
   exit(0);
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   if (argc > 1 && (*argv[1] == 'h' || strcmp(argv[1], "-h") == 0 ||
                    strcmp(argv[1], "--h") == 0))
     usage();
