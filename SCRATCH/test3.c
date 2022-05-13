b8a2740920238825edc4f67e71a6646933527bc8
alexbora <alexbora@gmail.com>
2022-05-13 16:48:40 +0300

    mix - vineri 13 mai 2022, 16:48:40 +0300

--- SCRATCH/test3.c
+++ SCRATCH/test3.c
@@ -4,12 +4,29 @@
  * @created     : marţi mai 10, 2022 19:21:17 EEST
  */
 
+#include <fcntl.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 static inline int fn1(void) { return 0; }
 
 int fn(void) { return fn1(); }
+
+void sys_error(char *error, ...) {
+  va_list argptr;
+  char string[1024];
+
+  // change stdin to non blocking
+  fcntl(0, F_SETFL, fcntl(0, F_GETFL, 0) & ~FNDELAY);
+
+  va_start(argptr, error);
+  vsprintf(string, error, argptr);
+  va_end(argptr);
+  fprintf(stderr, "Error: %s\n", string);
+
+  exit(1);
+}
 
 int main(int argc, char *argv[]) {
 
@@ -27,6 +44,8 @@ int main(int argc, char *argv[]) {
   struct work_s w2 = {{}};
 
   w1.route = (struct Route){"a", 1, NULL};
+
+  sys_error("error");
 
   return 0;
 }
