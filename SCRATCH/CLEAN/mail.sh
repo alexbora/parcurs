a739ab1e82d7d12b784b1d9ca24f043b42f13483
alex <alexbora@gmail.com>
2022-06-04 10:55:51 +0300

    t400

--- SCRATCH/CLEAN/mail.sh
+++ SCRATCH/CLEAN/mail.sh
@@ -24,8 +24,10 @@ sesMessage=$''
 sesFile="$1"
 sesMIMEType=`file --mime-type "$sesFile" | sed 's/.*: //'`
 
+./test.sh
 
 curl -v --url smtps://$sesSMTP:$sesPort --ssl-reqd  --mail-from $sesFromAddress --mail-rcpt $sesToAddress  --user $sesAccess:$sesSecret -F '=(;type=multipart/mixed' -F "=$sesMessage;type=text/plain" -F "file=@$sesFile;type=$sesMIMEType;encoder=base64" -F '=)' -H "Subject: $sesSubject" -H "From: $sesFromName <$sesFromAddress>" -H "To: $sesToName <$sesToAddress>"
 
+make clean
 
 # curl -v --url smtps://$sesSMTP:$sesPort --ssl-reqd  --mail-from $sesFromAddress --mail-rcpt $sesToAddress  --user $sesAccess:$sesSecret -F '=(;type=multipart/mixed' -F "=$sesMessage;type=text/plain" -F "file=@$sesFile;type=$sesMIMEType;encoder=base64" -F '=)' -H "Subject: $sesSubject" -H "From: $sesFromName <$sesFromAddress>" -H "To: $sesToName <$sesToAddress>"
