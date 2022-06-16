8077b1cedd7b9ac2a6096c10941b51ddaa33112d
alexbora <alexbora@gmail.com>
2022-06-16 21:04:48 +0300

    T460 - joi 16 iunie 2022, 21:04:48 +0300

--- SCRATCH/test9.c
+++ SCRATCH/test9.c
@@ -4,6 +4,7 @@
  * @created     : Miercuri Iun 15, 2022 08:08:23 EEST
  */
 
+#include "test9.h"
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,36 +13,21 @@
 #include <unistd.h>
 
 #ifdef LOG
-static int fd;
-#define INIT_FD  init_fd();
-#define CLOSE_FD close_fd();
-
-static void init_fd(void)
-{
-  fd = open("log", O_CREAT | O_APPEND | O_RDWR | O_TRUNC | O_EXLOCK, 0664);
+int fd;
+void init_fd(void) {
+  fd = open("log", O_CREAT | O_APPEND | O_RDWR | O_TRUNC | F_EXLCK, 0664);
 }
-
-static void close_fd(void)
-{
-  close(fd);
-}
-
-#else
-#define INIT_FD
-#define CLOSE_FD
-#define fd 2
+void close_fd(void) { close(fd); }
 #endif
 
-#define PRINT_(a) write(fd, a, strlen(a));
-
-int main()
-{
+int main() {
   /* fd = open("log", O_CREAT | O_APPEND | O_RDWR | O_TRUNC, 0666); */
   INIT_FD
   write(fd, "a\n", 2);
   write(fd, "b\n", 2);
   PRINT_("c\n")
   PRINT_("d\n")
+  f1();
   CLOSE_FD
 
   return 0;
