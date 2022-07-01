6ece8dea58f91f240f0d716662c5cb1ae408355b
alexbora <alexbora@gmail.com>
2022-07-01 20:36:00 +0300

    T460-vineri 1 iulie 2022, 20:36:00 +0300

--- SCRATCH/CLEAN/main.c
+++ SCRATCH/CLEAN/main.c
@@ -12,17 +12,19 @@ static int pm_qos_fd = -1;
 
 void start_low_latency(void) {
   int target = 0;
-  if (pm_qos_fd >= 0) return;
+  if (pm_qos_fd >= 0)
+    return;
   pm_qos_fd = open("/dev/cpu_dma_latency", O_RDWR);
   write(pm_qos_fd, &target, sizeof(target));
 }
 
 void stop_low_latency(void) {
-  if (pm_qos_fd >= 0) close(pm_qos_fd);
+  if (pm_qos_fd >= 0)
+    close(pm_qos_fd);
 }
 
-#define LOW_LATENCY start_low_latency( );
-#define NO_LATENCY stop_low_latency( );
+#define LOW_LATENCY start_low_latency();
+#define NO_LATENCY stop_low_latency();
 #else
 #define LOW_LATENCY
 #define NO_LATENCY
@@ -45,16 +47,15 @@ int main(int argc, char *argv[]) {
   INIT_FD
 
   LOW_LATENCY
+  mix();
 
   init_time(argc, argv);
 
-  mix( );
-
   get_km(argc > 3 ? argv[argc - 1] : NULL);
 
-  write_excel( );
+  write_excel();
 
-  write_km( );
+  write_km();
 
   mail_me(attachment);
 
