3eec16bbfc5f213f0697c8c2994cdf1cdfacbf93
alex <alexbora@gmail.com>
2022-07-05 18:48:29 +0300

    mac Mar  5 Iul 2022 18:48:29 EEST

---
+++ SCRATCH/test9_2.c
@@ -0,0 +1,16 @@
+/**
+ * @author      : alex (alexbora@gmail.com)
+ * @file        : test9_2
+ * @created     : Marţi Iul 05, 2022 17:58:54 EEST
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define W(x) (array[(x)&15])
+
+int main(int argc, char *argv[])
+{
+  W(10);
+  return 0;
+}
