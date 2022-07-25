2a5bf0abcda41aa6d3e1c6fb643c3974cd56fc9d
alex <alexbora@gmail.com>
2022-07-25 17:09:07 +0300

    feats - luni 25 iulie 2022, 17:09:07 +0300

---
+++ SCRATCH/RELEASE/div/asm_main1.c
@@ -0,0 +1,12 @@
+/**
+ * @author      : alex (alexbora@gmail.com)
+ * @file        : asm_main1
+ * @created     : luni iul 25, 2022 16:17:19 EEST
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int test(int);
+
+int main(int argc, char *argv[]) { return test(16); }
