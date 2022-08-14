57194028bd10407989a6ad6827dfc8da6902b5fb
alex <alexbora@gmail.com>
2022-08-14 16:13:05 +0300

    feats - duminică 14 august 2022, 16:13:05 +0300

--- SCRATCH/RELEASE/asm/arg5.s
+++ SCRATCH/RELEASE/asm/arg5.s
@@ -1,10 +1,25 @@
         .global _start
 
+txt:
+        .byte 97
+endtxt:
+
 _start:
+        pushq %rbp
+        movq %rsp, %rbp
+
         movb $1, %ah
         movb $3, %al
         movb $0, %al
         subb $1, %al
+
+        movq $(endtxt-txt), %rdi
+        popq %rbp
+
+        movb $4, %ah
+        movb $3, %al
+        rorl $8, %eax
+        movq %rax, %rdi
 
         movq $60, %rax
         syscall
