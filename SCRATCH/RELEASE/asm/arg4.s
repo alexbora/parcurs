1d570579aad0e206a56d4be9ebd660c4b89546dd
alex <alexbora@gmail.com>
2022-08-14 09:05:40 +0300

    mac

---
+++ SCRATCH/RELEASE/asm/arg4.s
@@ -0,0 +1,30 @@
+
+.globl _start
+
+        .data
+numbers:
+        .quad 1, 2, 3, 4, 0
+
+        .text
+
+_start:
+loopstart:
+        enter $16, $0
+        movq $0, %rcx
+        incq %rcx
+        movq numbers(, %rcx, 8), %rax
+        cmpq $2, %rax
+        je continueloop
+
+continueloop:
+        incq %rcx
+        movq numbers(, %rcx, 8), %rax
+endloop:
+        movb %al, %dil
+        leave
+
+        movq $60, %rax
+        syscall
+        ret
+        hlt
+
