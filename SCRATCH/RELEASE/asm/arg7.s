3081d88e5317e5a409c7369a22e1a89e40913496
alex <alexbora@gmail.com>
2022-08-15 18:02:56 +0300

    mac Lun 15 Aug 2022 18:02:56 EEST

--- SCRATCH/RELEASE/asm/arg7.s
+++ SCRATCH/RELEASE/asm/arg7.s
@@ -7,10 +7,13 @@ _start:
         movq %rsp, %rbp
 
         xorq %rax, %rax
-        movb $3, %bl
+        movb $0, %bl
         testb $0b00000010, %bl
         jnz bitwasset
 
+        movb $3, %bl
+        andb $0b0000011, %bl
+        movb %bl, %al
         leave
         ret
 
