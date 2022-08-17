8bbc94658dbe29b219ff914637aca206b8bc6968
alex <alexbora@gmail.com>
2022-08-17 21:50:48 +0300

    mac Mie 17 Aug 2022 21:50:48 EEST

--- SCRATCH/RELEASE/asm/arg1.s
+++ SCRATCH/RELEASE/asm/arg1.s
@@ -1,6 +1,12 @@
-        .globl _main
+        .globl _main, _fprintf
 
+        .data
+txt:
+        .ascii "txt\n\0"
+        .p2align 4
+        .text
 _main:
+        jmp print
         movl $1, %eax
         cpuid
         /* andb $0b11000000000000000000000000000, %ecx */
@@ -18,3 +24,14 @@ exit:
         movq $0, %rax
         ret
 
+_fprintf:
+print:
+        enter $0, $0
+        movq ___stderrp@GOTPCREL(%rip), %rdi
+        movq (%rdi), %rdi
+        leaq txt(%rip), %rsi
+        call *_fprintf@GOTPCREL(%rip)
+        /* call _fprintf */
+        /* xorq %rax, %rax */
+        leave
+        ret
---
+++ SCRATCH/RELEASE/asm/main6.c
@@ -0,0 +1,16 @@
+/**
+ * @author      : alex (alexbora@gmail.com)
+ * @file        : main6
+ * @created     : Marţi Aug 16, 2022 20:33:10 EEST
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+
+  fprintf(stderr, "%s\n", "a");
+
+  return 0;
+}
