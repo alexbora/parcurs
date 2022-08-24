93bddb9d536af67270ce4bc4b2dac488b0bd1d5a
alex <alexbora@gmail.com>
2022-08-24 10:12:02 +0300

    mac Mie 24 Aug 2022 10:12:02 EEST

---
+++ SCRATCH/RELEASE/asm/main14.c
@@ -0,0 +1,14 @@
+/**
+ * @author      : alex (alexbora@gmail.com)
+ * @file        : main14
+ * @created     : Marţi Aug 23, 2022 20:10:33 EEST
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  printf("%f\n", 2);
+  return 0;
+}
