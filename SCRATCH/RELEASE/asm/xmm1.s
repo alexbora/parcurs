46374aef2a3d2ef308df676b21bd68a66c070f38
alex <alexbora@gmail.com>
2022-08-26 10:45:27 +0300

    mac Vin 26 Aug 2022 10:45:27 EEST

---
+++ SCRATCH/RELEASE/asm/xmm1.s
@@ -0,0 +1,15 @@
+        .global _main
+
+_main:
+        movl $2, %eax
+        movq %rax, %xmm5
+_shitft:
+        pslldq $8, %xmm5
+        movq $3, %rax
+        movss %eax, %xmm5w
+
+_exit:
+        xorq %rax, %rax
+        movq %xmm5, %rax
+        ret
+
