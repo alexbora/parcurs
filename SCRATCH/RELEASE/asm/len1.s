828df6bd4fdde63d97fa4444645d37efe4ebb9d0
alex <alexbora@gmail.com>
2022-09-12 20:51:29 +0300

    mac Lun 12 Sep 2022 20:51:29 EEST

--- SCRATCH/RELEASE/asm/len1.s
+++ SCRATCH/RELEASE/asm/len1.s
@@ -6,11 +6,13 @@ string:
 
         .text
 _main:
-        leaq string(%rip), %rdi
         xorq %rax, %rax
+        leaq string(%rip), %rdi
         /* movb $'x', %al */
         /* repne scasb */
+        /* addl %edi, %r11d */
         repnz scasb
         decl %edi
         movl %edi, %eax
+        /* movl %r11d, %eax */
         ret
