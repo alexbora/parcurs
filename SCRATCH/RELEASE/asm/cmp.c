05e7758929844beab1c5a25aeec9401635a6f540
alex <alexbora@gmail.com>
2022-09-17 18:34:44 +0000

    feats - sâmbătă 17 septembrie 2022, 21:34:44 +0300

---
+++ SCRATCH/RELEASE/asm/cmp.c
@@ -0,0 +1,10 @@
+/**
+ * @author      : alex (alexbora@gmail.com)
+ * @file        : cmp
+ * @created     : Saturday Sep 17, 2022 18:32:13 UTC
+ */
+
+int main(int argc, char *argv[]) {
+  int x = '*';
+  return x == '*' ?: -1;
+}
