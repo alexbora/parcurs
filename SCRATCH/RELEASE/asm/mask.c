5256998958bec792358dcc3d013c6d2e3066c464
alex <alexbora@gmail.com>
2022-09-20 19:47:16 +0300

    mac Mar 20 Sep 2022 19:47:16 EEST

---
+++ SCRATCH/RELEASE/asm/mask.c
@@ -0,0 +1,23 @@
+/**
+ * @author      : alex (alexbora@gmail.com)
+ * @file        : mask
+ * @created     : Marţi Sep 20, 2022 19:36:35 EEST
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  uint_fast64_t x = 4;
+  printf("%llu\n", -(unsigned)0);
+
+  printf("%x\n", ~x);
+x  &= ~mask) | (2 & mask);
+0b11111110 0b11111111 -- -- -- -- --0b11111110
+
+    0b11111110 0b11111111 -- -- -- -- --0b11111110
+
+    return 0;
+}
