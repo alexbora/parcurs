c1e3dcee7d4fe55898df540417080f152757df85
alex <alexbora@gmail.com>
2022-09-30 21:41:34 +0300

    mac Vin 30 Sep 2022 21:41:34 EEST

---
+++ SCRATCH/RELEASE/asm/hex2.c
@@ -0,0 +1,16 @@
+/**
+ * @author      : alex (alexbora@gmail.com)
+ * @file        : hex2
+ * @created     : Vineri Sep 30, 2022 21:32:11 EEST
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  (void)argc;
+  (void)argv;
+  printf("%d\n", 0x10 << 1);
+  return 0;
+}
