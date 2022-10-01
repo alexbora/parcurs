7ac12e464820ba7363007bea09f55d829955021a
alex <alexbora@gmail.com>
2022-10-01 15:08:46 +0300

    mac Sâm  1 Oct 2022 15:08:46 EEST

---
+++ SCRATCH/RELEASE/asm/rand2.c
@@ -0,0 +1,14 @@
+/**
+ * @author      : alex (alexbora@gmail.com)
+ * @file        : rand2
+ * @created     : Sâmbătă Oct 01, 2022 14:52:16 EEST
+ */
+
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+  int x = random() % 2;
+  return 0;
+}
