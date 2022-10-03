62f832b2002f73cd1139ff5d143131c95980ec0e
alex <alexbora@gmail.com>
2022-10-03 21:27:29 +0300

    mac Lun  3 Oct 2022 21:27:29 EEST

--- SCRATCH/RELEASE/asm/vector1.c
+++ SCRATCH/RELEASE/asm/vector1.c
@@ -12,12 +12,21 @@
 #include <stdlib.h>
 #include <xmmintrin.h>
 
-int main(int argc, char *argv[]) {
-  __m128i arr;
+int main(int argc, char *argv[])
+{
+  __m128i                 arr;
   alignas(16) const float x[10] = {2};
-  arr = _mm_loadu_ps(x);
+  arr                           = _mm_loadu_ps(x);
 
   __m128i y = {1, 2};
+
+  union {
+    alignas(16) int x[2];
+    __m128i v;
+  } u;
+
+  u.v = (__m128i){1, 2};
+  printf("%d\n", u.x[0]);
 
   return 0;
 }
