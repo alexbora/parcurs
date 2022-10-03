42e26bcf22ded7b669288261e89ef3122c802b88
alex <alexbora@gmail.com>
2022-10-03 21:59:40 +0300

    mac Lun  3 Oct 2022 21:59:40 EEST

--- SCRATCH/RELEASE/asm/vector1.c
+++ SCRATCH/RELEASE/asm/vector1.c
@@ -28,5 +28,17 @@ int main(int argc, char *argv[])
   u.v = (__m128i){1, 2};
   printf("%d\n", u.x[0]);
 
+  __m128i v2 = {3, 4};
+  printf("%lld\n", v2[0]);
+
+  alignas(16) union {
+    alignas(16) long long z[4];
+    __m128i v3[2];
+  } u2;
+
+  u2.v3[0] = (__m128i){1, 7};
+  u2.v3[1] = (__m128i){2, 6};
+  printf("%lld\n", u2.z[2]);
+
   return 0;
 }
