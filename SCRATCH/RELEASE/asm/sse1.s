61a0e1b432d0a3d33c59d776a2e0004a02bf0954
alex <alexbora@gmail.com>
2022-10-05 18:12:57 +0300

    mac Mie  5 Oct 2022 18:12:57 EEST

--- SCRATCH/RELEASE/asm/sse1.s
+++ SCRATCH/RELEASE/asm/sse1.s
@@ -3,10 +3,10 @@
         .p2align 4
         .data
 val:
-        .double 75.5
+        .quad 9
         .p2align 4
 val2:
-        .quad 1, 2
+        .quad 3
 
         .p2align 4
         .text
@@ -15,4 +15,12 @@ _main:
         movq $5, %rax
         cvtsi2sd %rax, %xmm0
         movdqa val2(%rip), %xmm0
+
+        movq val(%rip), %xmm1
+        pslldq $8, %xmm1
+        movsd val2(%rip), %xmm1
+        movsd %xmm1, %xmm6
+        psrldq $8, %xmm1
+        movq %xmm1, %rax
+
         ret
