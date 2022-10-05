e6c3ed2448167669c1c76ac3ae9961aa5ebe59ad
alex <alexbora@gmail.com>
2022-10-05 16:35:05 +0000

    feats - miercuri 5 octombrie 2022, 19:35:05 +0300

--- SCRATCH/RELEASE/asm/sse1.s
+++ SCRATCH/RELEASE/asm/sse1.s
@@ -1,4 +1,4 @@
-        .global _main
+        .global main
 
         .p2align 4
         .data
@@ -10,17 +10,15 @@ val2:
 
         .p2align 4
         .text
-_main:
-        movq val(%rip), %xmm0
-        movq $5, %rax
-        cvtsi2sd %rax, %xmm0
-        movdqa val2(%rip), %xmm0
+main:
+        //movq val(%rip), %xmm0
+        //movq $5, %rax
+        //cvtsi2sd %rax, %xmm0
+        //movdqa val2(%rip), %xmm0
 
-        movq val(%rip), %xmm1
-        pslldq $8, %xmm1
-        movsd val2(%rip), %xmm1
-        movsd %xmm1, %xmm6
-        psrldq $8, %xmm1
-        movq %xmm1, %rax
-
+        movq val(%rip), %xmm6
+        pslldq $8, %xmm6
+        movsd val(%rip), %xmm6
+        psrldq $8, %xmm6
+        movq %xmm6, %rax
         ret
