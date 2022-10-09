94f2a0a2da1f580c9f8b7670a3cfc29045c2aebd
alex <alexbora@gmail.com>
2022-10-09 05:36:11 +0000

    feats - duminică 9 octombrie 2022, 08:36:11 +0300

--- SCRATCH/RELEASE/asm/mutex1.c
+++ SCRATCH/RELEASE/asm/mutex1.c
@@ -6,10 +6,36 @@
 
 #include <pthread.h>
 #include <stdlib.h>
-#include <sys/_pthread/_pthread_mutex_t.h>
+/* #include <sys/_pthread/_pthread_mutex_t.h> */
+#include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
-{
+pthread_mutex_t m1 = PTHREAD_MUTEX_INITIALIZER;
+
+void *f1(void *p) {
+  sleep(3);
+  pthread_mutex_unlock(&m1);
+  puts("f1\n");
+  return NULL;
+}
+
+void *f2(void *p) {
+  sleep(5);
+  puts("f2\n");
+  return NULL;
+}
+
+int main(int argc, char *argv[]) {
+  pthread_mutex_lock(&m1);
+
+  pthread_t t1;
+  pthread_create(&t1, NULL, f2, NULL);
+  f1(NULL);
+
+  pthread_join(t1, NULL);
+
+  return 0;
+
   pthread_mutex_t *mtx = NULL;
   pthread_mutex_init(mtx, NULL);
   return 0;
