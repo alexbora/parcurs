88d1edce073eea20c427a361aa84f2cf3797fe9f
alex <alexbora@gmail.com>
2022-10-15 14:05:14 +0000

    feats - sâmbătă 15 octombrie 2022, 17:05:14 +0300

--- SCRATCH/RELEASE/asm/tmpfile1.c
+++ SCRATCH/RELEASE/asm/tmpfile1.c
@@ -11,13 +11,17 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  int fd = open("tmp1.txt", O_RDWR | O_CREAT);
+  /* int fd = open("tmp1.xlsx", O_RDWR | O_CREAT); */
   char *buffer = malloc(1024);
-  FILE *f = fmemopen(buffer, 64, "w++");
-  setbuf(f, buffer);
-  fprintf(f, "%s\n", "test");
+  /* FILE *f = fmemopen(buffer, 64, "w++"); */
+  FILE *x = fopen("tmp2.xlsx", "w++");
+
+  // setbuf(x, buffer);
+  setbuffer(x, buffer, 64 * 1014);
+  fprintf(x, "%s\n", "test___");
+  lxw_workbook *w = workbook_new_opt(buffer, NULL);
+  workbook_add_worksheet(w, "1");
   printf("%s\n", buffer);
-  lxw_workbook *w = workbook_new(buffer);
   workbook_close(w);
 
   return 0;
