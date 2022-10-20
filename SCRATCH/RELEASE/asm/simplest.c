5be35acaabc0bcb19c74f71a5f9655175964a4bd
alex <alexbora@gmail.com>
2022-10-20 13:47:47 +0300

    mac Joi 20 Oct 2022 13:47:47 EEST

---
+++ SCRATCH/RELEASE/asm/simplest.c
@@ -0,0 +1,19 @@
+/**
+ * @author      : alex (alexbora@gmail.com)
+ * @file        : simplest
+ * @created     : Joi Oct 20, 2022 13:31:51 EEST
+ */
+
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+  int fd =
+      open("/Users/alex/dev/parcurs/SCRATCH/RELEASE/asm/yyy", O_CREAT | O_RDWR);
+  write(fd, "test", 4);
+  close(fd);
+  return 0;
+}
