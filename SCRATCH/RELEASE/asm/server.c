38ffc58c5473af17fd0084e18af25bdf18bd2bc1
alex <alexbora@gmail.com>
2022-10-26 21:59:44 +0300

    T400-2

--- SCRATCH/RELEASE/asm/server.c
+++ SCRATCH/RELEASE/asm/server.c
@@ -4,5 +4,26 @@
  * @created     : miercuri oct 26, 2022 20:49:14 EEST
  */
 
+#define _GNU_SOURCE
+
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/sendfile.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void *foo(void *a) { return NULL; }
+
+int main(int argc, char *argv[]) {
+
+  pthread_t t1;
+  pthread_create(&t1, NULL, foo, NULL);
+
+  return EXIT_SUCCESS;
+}
