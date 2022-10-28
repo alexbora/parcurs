42383edac169f794df6a66c186d6fa21858aefd2
alex <alexbora@gmail.com>
2022-10-28 05:14:26 +0300

    mac Vin 28 Oct 2022 05:14:26 EEST

--- SCRATCH/RELEASE/asm/server.c
+++ SCRATCH/RELEASE/asm/server.c
@@ -26,11 +26,13 @@
   {                                                                            \
   }
 #else
-FILE*         error_log;
+FILE         *error_log;
 unsigned char error_mode;
 #define STOP_IF(assertion, error_action, ...)                                  \
   {                                                                            \
-    if (assertion) { fprintf(error_log ? error_log : stderr, __VA_ARGS__); }   \
+    if (assertion) {                                                           \
+      fprintf(error_log ? error_log : stderr, __VA_ARGS__);                    \
+    }                                                                          \
     if (error_mode == 'x') {                                                   \
       abort();                                                                 \
     } else {                                                                   \
@@ -42,37 +44,50 @@ unsigned char error_mode;
 pthread_t t1;
 int       sock;
 
-void*
-foo(void* in)
+void *foo(void *in)
 {
-  int  sockfd = *(int*) in;
+  int  sockfd = *(int *)in;
   char client_request[BUFSIZ];
   recv(sockfd, client_request, 8192, 0);
   puts("connected\n");
   return NULL;
 }
 
-int
-main(int argc, char* argv[])
+void getip(int sock, int *ip)
 {
+  socklen_t          addr_size = sizeof(struct sockaddr_in);
+  struct sockaddr_in addr;
+  getsockname(sock, (struct sockaddr *)&addr, &addr_size);
 
-  sock                            = socket(AF_INET, SOCK_STREAM, 0);
+  char *host = inet_ntoa(addr.sin_addr);
+  sscanf(host, "%d.%d.%d.%d", &ip[0], &ip[1], &ip[2], &ip[3]);
+}
+
+int main(int argc, char *argv[])
+{
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &(int[]){1}, sizeof(int));
+
   const struct sockaddr_in server = {.sin_family      = AF_INET,
                                      .sin_addr.s_addr = INADDR_ANY,
                                      .sin_port        = htons(8080)};
 
   struct sockaddr_in client = {};
 
-  int b = bind(sock, (const struct sockaddr*) &server, sizeof(server));
+  int b = bind(sock, (const struct sockaddr *)&server, sizeof(server));
 
   STOP_IF(b < 0, return -1, "could not bind to socket\n");
 
   listen(sock, 3);
+
   while (1) {
-    accept(sock, (struct sockaddr*) &client,
-           (socklen_t*) (sizeof(struct in_addr)));
+    int conn = accept(sock, (struct sockaddr *)&client,
+                      (socklen_t *)(sizeof(struct in_addr)));
+    write(conn, "200 \n", 5);
+
     pthread_create(&t1, NULL, foo, &sock);
     pthread_join(t1, NULL);
+    int pid = fork();
   }
 
   return EXIT_SUCCESS;
