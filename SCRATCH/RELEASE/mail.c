200164c7de177381f93b36b62d4913b47d30c90f
alex <alexbora@gmail.com>
2022-11-02 16:46:42 +0200

    mac Mie  2 Noi 2022 16:46:42 EET

--- SCRATCH/RELEASE/mail.c
+++ SCRATCH/RELEASE/mail.c
@@ -54,26 +54,32 @@ static inline int upload_m(SSL *s, const char *const filename)
   size_t in_size;
   int    fd = open(filename, O_RDONLY);
   size_t file_size;
-  {
-    int         pagesize = getpagesize();
-    struct stat fs;
-    fstat(fd, &fs);
-    file_size = fs.st_size;
-    in_size   = fs.st_size;
-    in_size += fs.st_size & ~(pagesize - 1);
-    close(fd);
-  }
+
+  int         pagesize = getpagesize();
+  struct stat fs;
+  fstat(fd, &fs);
+  file_size = fs.st_size;
+  in_size   = fs.st_size;
+  in_size += fs.st_size & ~(pagesize - 1);
+  /* close(fd); */
+
   unsigned char *x = mmap(0, in_size, PROT_READ, MAP_PRIVATE, fd, 0);
 
 #define ENC_LEN(_n_) ((_n_ + 2) / 3 * 4)
 
+  /* int dst = open("map2.txt", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+   */
+
   size_t out_size = ENC_LEN(in_size);
+  /* ftruncate(dst, out_size); */
 
   unsigned char *out =
       mmap(0, out_size, PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
 #undef ENC_LEN
   const int out_len = EVP_EncodeBlock(out, x, file_size);
+  close(fd);
+  munmap(x, in_size);
   return SSL_write(s, out, out_len);
 }
 
--- SCRATCH/RELEASE/mail_tmp.c
+++ SCRATCH/RELEASE/mail_tmp.c
@@ -21,38 +21,35 @@ static inline int upload_m(SSL *s, const char *const filename)
   int    fd = open(filename, O_RDWR);
   size_t file_size;
 
-  /* int         pagesize = getpagesize(); */
+  int         pagesize = getpagesize();
   struct stat fs;
   fstat(fd, &fs);
   int in_size = fs.st_size;
-  /* in_size += fs.st_size & ~(pagesize - 1); */
+  in_size += fs.st_size & ~(pagesize - 1);
   close(fd);
   size_t out_size = ENC_LEN(in_size);
 
   int dst = open("map2.txt", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+  ftruncate(dst, out_size);
 
   unsigned char *x = mmap(0, in_size, PROT_READ, MAP_PRIVATE, fd, 0);
   unsigned char *out =
       mmap(0, out_size, PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
-  ftruncate(dst, out_size);
-
-  unsigned char *out_ex = malloc(out_size);
+  /* unsigned char *out_ex = malloc(out_size); */
   /* memcpy(out_ex, x, 10); */
-  puts("out_ex x works");
+  /* puts("out_ex x works"); */
   /* memcpy(out, "x", 1); */
-  puts("out \'x' works");
-  memcpy(out, x, 1);
+  /* puts("out \'x' works"); */
+  memcpy(out, x, in_size);
   msync(out, out_size, MS_SYNC);
   munmap(x, in_size);
   munmap(out, out_size);
   puts("out x works");
-  return 1;
 
-  unsigned char *y = malloc(out_size);
-
-  const int out_len = EVP_EncodeBlock(y, x, file_size);
-  return SSL_write(s, y, out_len);
+  const int _len = EVP_EncodeBlock(out, x, file_size);
+  /* return SSL_write(s, y, out_len); */
+  return 0;
 }
 
 int main(void)
