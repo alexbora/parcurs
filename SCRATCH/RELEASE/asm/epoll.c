8e7b970fb8078d8b778df090efdd99427663b137
alex <alexbora@gmail.com>
2022-11-05 13:34:47 +0200

    mac Sâm  5 Noi 2022 13:34:47 EET

---
+++ SCRATCH/RELEASE/asm/epoll.c
@@ -0,0 +1,27 @@
+/**
+ * @author      : alex (alexbora@gmail.com)
+ * @file        : epoll
+ * @created     : Sâmbătă Noi 05, 2022 13:30:40 EET
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/event.h>
+
+union attr {
+  struct is {
+    int def;
+  } boolt;
+  struct data {
+    unsigned def;
+  } data;
+};
+
+int main(int argc, char *argv[])
+{
+  union attr a;
+  a.boolt.def = 7;
+  printf("%d\n", a.data.def);
+
+  return 0;
+}
