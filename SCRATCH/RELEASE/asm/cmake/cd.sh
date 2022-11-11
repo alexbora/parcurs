fc4df6ae6af8f0f8f4dea2b1659a135a45974d13
alex <alexbora@gmail.com>
2022-11-11 18:35:54 +0200

    mac Vin 11 Noi 2022 18:35:54 EET

---
+++ SCRATCH/RELEASE/asm/cmake/cd.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : alex (alex@mac.local)
+# @file        : cd
+# @created     : Vineri Noi 11, 2022 18:34:33 EET
+#
+# @description : 
+######################################################################
+
+
+mkdir build && cd build 
+cmake .. 
