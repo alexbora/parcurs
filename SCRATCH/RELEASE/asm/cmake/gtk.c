c9df7ee46d42fb0793760e36495b20ef407f7284
alex <alexbora@gmail.com>
2022-11-12 18:30:05 +0200

    mac Sâm 12 Noi 2022 18:30:05 EET

--- SCRATCH/RELEASE/asm/cmake/gtk.c
+++ SCRATCH/RELEASE/asm/cmake/gtk.c
@@ -18,7 +18,8 @@ static void activate(GtkApplication *app, gpointer user_data)
 int main(int argc, char **argv)
 {
   GtkApplication *app =
-      gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+      /* gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS); */
+      gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
   g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   int status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
