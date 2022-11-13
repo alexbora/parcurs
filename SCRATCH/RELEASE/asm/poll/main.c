20bd165053c1dbe2ba4104ac6237fa410a6bc4d7
alex <alexbora@gmail.com>
2022-11-13 09:54:47 +0200

    mac Dum 13 Noi 2022 09:54:47 EET

---
+++ SCRATCH/RELEASE/asm/poll/main.c
@@ -0,0 +1,16 @@
+/**
+ * @author      : alex (alexbora@gmail.com)
+ * @file        : main
+ * @created     : Duminică Noi 13, 2022 09:42:17 EET
+ */
+
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void)
+{
+
+  return EXIT_SUCCESS;
+}
