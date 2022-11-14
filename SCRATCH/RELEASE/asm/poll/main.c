e232b2e3bc99693bd95f1534abca11ffbab94740
alex <alexbora@gmail.com>
2022-11-14 09:15:41 +0200

    mac Lun 14 Noi 2022 09:15:41 EET

--- SCRATCH/RELEASE/asm/poll/main.c
+++ SCRATCH/RELEASE/asm/poll/main.c
@@ -4,15 +4,22 @@
  * @created     : Duminică Noi 13, 2022 09:42:17 EET
  */
 
+#include <fcntl.h>
 #include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/fcntl.h>
 #include <sys/poll.h>
 #include <unistd.h>
 
 int main(void)
 {
-  struct pollfd fd[2];
+  struct pollfd fd;
+
+  int f = open("test.txt", O_CREAT | O_RDWR);
+
+  fd.fd     = f;
+  fd.events = POLLIN;
 
   return EXIT_SUCCESS;
 }
