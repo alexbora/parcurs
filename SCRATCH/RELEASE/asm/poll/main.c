484366468c917460ec7f002cd91acb44e302860c
alex <alexbora@gmail.com>
2022-11-14 17:09:05 +0200

    mac Lun 14 Noi 2022 17:09:05 EET

--- SCRATCH/RELEASE/asm/poll/main.c
+++ SCRATCH/RELEASE/asm/poll/main.c
@@ -16,10 +16,19 @@ int main(void)
 {
   struct pollfd fd;
 
-  int f = open("test.txt", O_CREAT | O_RDWR);
+  int f = open("test.txt", O_TRUNC | O_CREAT | O_RDWR, 0666);
 
   fd.fd     = f;
   fd.events = POLLIN;
 
+  poll(&fd, 1, 1000);
+
+  if (fd.revents & POLLIN)
+    printf("stdin is readable\n");
+  if (fd.revents & POLLOUT)
+    printf("stdout is writable\n");
+  write(f, "1\n", 2);
+
+  close(f);
   return EXIT_SUCCESS;
 }
