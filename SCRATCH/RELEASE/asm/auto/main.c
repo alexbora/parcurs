453d10216edd6139b0d29f7bb36a10d538a2578d
alex <alexbora@gmail.com>
2022-11-15 14:55:12 +0200

    mac Mar 15 Noi 2022 14:55:12 EET

---
+++ SCRATCH/RELEASE/asm/auto/main.c
@@ -0,0 +1,15 @@
+/**
+ * @author      : alex (alexbora@gmail.com)
+ * @file        : main
+ * @created     : Marţi Noi 15, 2022 14:32:25 EET
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+
+  puts("SUCCESS\n");
+  return EXIT_SUCCESS;
+}
