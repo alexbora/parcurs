f32e02375d8b14168351b4d5e2e79a4dfe914498
alex <alexbora@gmail.com>
2022-11-16 16:06:59 +0200

    mac Mie 16 Noi 2022 16:06:59 EET

---
+++ SCRATCH/RELEASE/asm/auto/bin/m1.c
@@ -0,0 +1,13 @@
+/**
+ * @author      : alex (alexbora@gmail.com)
+ * @file        : m1
+ * @created     : Miercuri Noi 16, 2022 15:59:35 EET
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+  puts("cmake");
+  return 0;
+}
