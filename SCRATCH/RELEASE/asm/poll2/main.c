9393000680a709415f9482a52c9e889f0a066718
alex <alexbora@gmail.com>
2022-11-16 12:34:36 +0200

    mac

---
+++ SCRATCH/RELEASE/asm/poll2/main.c
@@ -0,0 +1,14 @@
+/**
+ * @author      : alex (alexbora@gmail.com)
+ * @file        : main
+ * @created     : Marţi Noi 15, 2022 20:20:37 EET
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  puts("success");
+  return EXIT_SUCCESS;
+}
