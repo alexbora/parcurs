950f2287327a43c2dbc43ec30ff4d0b7f2622c0c
alex <alexbora@gmail.com>
2023-05-05 17:50:47 +0300

    mac Vin  5 Mai 2023 17:50:47 EEST

--- SCRATCH/CLEAN/fltk.cpp
+++ SCRATCH/CLEAN/fltk.cpp
@@ -5,6 +5,8 @@
  */
 
 #ifdef USE_GUI
+#include <FL/Fl.H>
+#include <FL/Fl_Double_Window.H>
 #include <FL/Fl_Input.H>
 #include <FL/Fl_Window.H>
 #include <cstdio>
@@ -13,10 +15,21 @@ int
 mainx(int, char**);
 }
 
+Fl_Window*
+make_window(int* dimensions, const char* label)
+{
+  Fl_Window* w = new Fl_Double_Window(dimensions[0], dimensions[1], label);
+  w->show();
+  return w;
+}
+
 int
 main(int argc, char* argv[])
 {
 
+  int        dim[2] = {800, 400};
+  Fl_Window* w      = make_window(dim, NULL);
+  Fl::run();
   printf("%s, %s, %s\n", argv[1], argv[2], argv[3]);
 
   return 0;
